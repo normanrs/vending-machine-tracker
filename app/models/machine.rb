bee2a4378669ccfab6efbd67adc47b7f17ab643e
normanrs <normanrs@live.com>
2018-10-26 10:43:04 -0600

    iteration 2

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,10 @@
 class Machine < ApplicationRecord
   validates_presence_of :location
-  
   belongs_to :owner
+  has_many :snacks
+
+  def average_price
+    snacks.average(:price).to_i.round(2)
+  end
+
 end
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,12 +1,41 @@
 require 'rails_helper'
 
 feature 'When a user visits a vending machine show page' do
+  before(:each) do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+    @snack_1 = @dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    @snack_2 = @dons.snacks.create(name: "Pop Rocks", price: 1.50)
+    @snack_3 = @dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+
+  end
+
   scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
-
-    visit machine_path(dons)
-
+    visit machine_path(@dons)
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see all the snacks for a machine' do
+    visit machine_path(@dons)
+    save_and_open_page
+    expect(page).to have_content(@snack_1.name)
+    expect(page).to have_content(@snack_1.price)
+    expect(page).to have_content(@snack_2.name)
+    expect(page).to have_content(@snack_2.price)
+    expect(page).to have_content(@snack_3.name)
+    expect(page).to have_content(@snack_3.price)
+  end
+
+  scenario 'they see the average snack price' do
+    visit machine_path(@dons)
+    save_and_open_page
+    expect(page).to have_content(@snack_1.name)
+    expect(page).to have_content(@snack_1.price)
+    expect(page).to have_content(@snack_2.name)
+    expect(page).to have_content(@snack_2.price)
+    expect(page).to have_content(@snack_3.name)
+    expect(page).to have_content(@snack_3.price)
+  end
+
+
 end
